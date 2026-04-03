602d9e2efec342735cb9d064168232b7fb97d009
ALER\Administrator <lerbusi@gmail.com>
2026-04-03 16:07:23 +0800

    for gh page deploy commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/ler-cm/",
   plugins: [vue()],
-})
+});
